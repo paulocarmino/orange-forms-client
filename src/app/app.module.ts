6df19b0a97af0f0d677c497c53e20cc4d801d40c
Paulo Carmino <contato@paulocarmino.com>
2018-02-15 15:05:07 -0200

    Add Form Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { RouterModule } from '@angular/router';
 import { routing } from './app.routing';
 import { HttpModule } from '@angular/http';
 import { Angular2TokenService } from 'angular2-token';
+import { FormService } from './shared/form.service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,7 @@ import { Angular2TokenService } from 'angular2-token';
     RouterModule,
     routing
   ],
-  providers: [Angular2TokenService],
+  providers: [Angular2TokenService, FormService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
